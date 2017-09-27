688e1cdcb01ae995d636ad4ed2901c22577f942a
Yihong Tian <belltian@amazon.com>
2017-09-27 14:48:00 -0700

    Updated with AWS sdk

--- app/src/main/java/com/example/belltian/myapplication/DisplayOrderList.java
+++ app/src/main/java/com/example/belltian/myapplication/DisplayOrderList.java
@@ -1,18 +1,4 @@
 package com.example.belltian.myapplication;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.amazonaws.services.sns.AmazonSNSClient;
 
-/**
- * Created by belltian on 9/27/17.
- */
-
-public class DisplayOrderList {
-
-    public void showList() {
-        List<String> testList = new ArrayList<>();
-        testList.add("1. order 1");
-        testList.add("2. order 2");
-        testList.add("3. order 3");
-    }
-}
--- app/src/main/java/com/example/belltian/myapplication/MainActivity.java
+++ app/src/main/java/com/example/belltian/myapplication/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.belltian.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.orderList);
+        final String[] order = new String[4];
+        for (int i = 0; i < order.length; i++) {
+            order[i] = i + " : Your Order" + i;
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, order);
+        listView.setAdapter(adapter);
     }
-
-    
-
 }
