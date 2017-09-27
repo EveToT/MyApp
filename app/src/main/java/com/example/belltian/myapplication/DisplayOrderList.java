b4595942b799eccd84b103d1a3bb05103c662701
Yihong Tian <belltian@amazon.com>
2017-09-27 11:27:06 -0700

    Initial commit

---
+++ app/src/main/java/com/example/belltian/myapplication/DisplayOrderList.java
@@ -0,0 +1,18 @@
+package com.example.belltian.myapplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by belltian on 9/27/17.
+ */
+
+public class DisplayOrderList {
+
+    public void showList() {
+        List<String> testList = new ArrayList<>();
+        testList.add("1. order 1");
+        testList.add("2. order 2");
+        testList.add("3. order 3");
+    }
+}
